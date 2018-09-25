f1b9fc92b079dd5cdeb7a284c97c38795dc05005
fabrossmann <fabian.rossmann@htlpinkafeld.at>
2018-09-25 19:09:46 +0200

    added Startsite,TImeline(Resume), Design

---
+++ resources/js/app.js
@@ -0,0 +1,4 @@
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', '/resources/js/app.js', function() {
+    console.log('callback - particles.js config loaded');
+  });
