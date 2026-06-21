5297a2b725bcc95b383e6b3f265808d3242432d1
MojangPlsFix <fabrossmann@gmail.com>
2026-06-21 19:43:22 +0200

    Add sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,15 @@ import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://fabrossmann.github.io',
+
   vite: {
     plugins: [tailwindcss()],
   },
 
-  integrations: [react()],
+  integrations: [react(), sitemap()],
 });
