9c91c1188cd7149ad1e6b5949120d0fee52b4989
MojangPlsFix <fabrossmann@gmail.com>
2026-06-21 16:42:14 +0200

    Add React integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,13 @@
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
-	vite: {
-		plugins: [tailwindcss()],
+  vite: {
+      plugins: [tailwindcss()],
 	},
+
+  integrations: [react()],
 });
